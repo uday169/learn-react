11f561bea24707b5b54e97d81876678fc00dab4f
Uday Pratap <udaypratap169@gmail.com>
2021-08-12 15:07:53 +0530

    expense component cerated

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const expenseItems = props.expenses;
   return (
-    <div className="expense-item">
-      <div>Aug 12th 2021</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
-      </div>
+    <div>
+      {expenseItems.map((item, index) => (
+        <div className="expense-item">
+          <ExpenseDate date={item.date} />
+          <div className="expense-item__description">
+            <h2>{item.title}</h2>
+            <div className="expense-item__price">${item.amount}</div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
